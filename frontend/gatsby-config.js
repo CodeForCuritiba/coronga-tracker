1cf675d9a125d9baf4658f719baff46121aec40e
Adão Lima <adaoolima@gmail.com>
2020-09-06 14:25:07 -0300

    plugin to load script

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -30,5 +30,12 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-load-script',
+      options: {
+        src: '/getGeoLocation.js', // Change to the script filename
+        async: true,
+      },
+    },
   ],
 }
