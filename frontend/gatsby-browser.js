d9a27c9c4436dfca1b0ed86fcea3f067ec055f86
Adão Lima <adaoolima@gmail.com>
2020-09-06 14:25:28 -0300

    Comment test browser apis

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -4,4 +4,16 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+// // You can delete this file if you're not using it
+// exports.onClientEntry = () => {
+//   console.log("We've started!");
+// }
+
+// exports.onInitialClientRender = () => {
+//   console.log("ReactDOM.render has executed");
+// }
+// //
+// exports.onRouteUpdate = ({ location, prevLocation }) => {
+//   console.log('new pathname', location.pathname)
+//   console.log('old pathname', prevLocation ? prevLocation.pathname : null)
+// }
