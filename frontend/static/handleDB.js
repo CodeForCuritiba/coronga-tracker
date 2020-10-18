79d124765e6a4e5de89ceb4469c2ae5d6e426beb
Adão Lima <adaoolima@gmail.com>
2020-10-18 16:57:05 -0300

    API_URL variable and saveCheckPoint method

--- frontend/static/handleDB.js
+++ frontend/static/handleDB.js
@@ -18,6 +18,8 @@ const DB_NAME = 'coronga-tracker';
 const DB_VERSION = 1; // Use a long long for this value (don't use a float)
 const DB_STORE_NAME = 'geolocation';
 
+const API_URL = 'https://hxf2nx257b.execute-api.sa-east-1.amazonaws.com/prod/history';
+
 var db;
 
 // Used to keep track of which view is displayed to avoid uselessly reloading it
@@ -49,7 +51,6 @@ function openDb() {
   };
 }
 
-
 /**
  * @param {string} store_name
  * @param {string} mode either "readonly" or "readwrite"
@@ -72,90 +73,6 @@ function clearObjectStore(store_name) {
   };
 }
 
-// function getBlob(key, store, success_callback) {
-//   var req = store.get(key);
-//   req.onsuccess = function(evt) {
-//     var value = evt.target.result;
-//     if (value)
-//       success_callback(value.blob);
-//   };
-// }
-
-/**
- * @param {IDBObjectStore=} store
- */
-function displayPubList(store) {
-  console.log("displayPubList");
-
-  if (typeof store == 'undefined')
-    store = getObjectStore(DB_STORE_NAME, 'readonly');
-
-  var pub_msg = $('#pub-msg');
-  pub_msg.empty();
-  var pub_list = $('#pub-list');
-  pub_list.empty();
-  // Resetting the iframe so that it doesn't display previous content
-  newViewerFrame();
-
-  var req;
-  req = store.count();
-  // Requests are executed in the order in which they were made against the
-  // transaction, and their results are returned in the same order.
-  // Thus the count text below will be displayed before the actual pub list
-  // (not that it is algorithmically important in this case).
-  req.onsuccess = function(evt) {
-    pub_msg.append('<p>There are <strong>' + evt.target.result +
-                    '</strong> record(s) in the object store.</p>');
-  };
-  req.onerror = function(evt) {
-    console.error("add error", this.error);
-    displayActionFailure(this.error);
-  };
-
-  var i = 0;
-  req = store.openCursor();
-  req.onsuccess = function(evt) {
-    var cursor = evt.target.result;
-
-    // If the cursor is pointing at something, ask for the data
-    if (cursor) {
-      console.log("displayPubList cursor:", cursor);
-      req = store.get(cursor.key);
-      req.onsuccess = function (evt) {
-        var value = evt.target.result;
-        var list_item = $('<li>' +
-                          '[' + cursor.key + '] ' +
-                          '(biblioid: ' + value.biblioid + ') ' +
-                          value.title +
-                          '</li>');
-        if (value.year != null)
-          list_item.append(' - ' + value.year);
-
-        if (value.hasOwnProperty('blob') &&
-            typeof value.blob != 'undefined') {
-          var link = $('<a href="' + cursor.key + '">File</a>');
-          link.on('click', function() { return false; });
-          link.on('mouseenter', function(evt) {
-                    setInViewer(evt.target.getAttribute('href')); });
-          list_item.append(' / ');
-          list_item.append(link);
-        } else {
-          list_item.append(" / No attached file");
-        }
-        pub_list.append(list_item);
-      };
-
-      // Move on to the next object in store
-      cursor.continue();
-
-      // This counter serves only to create distinct ids
-      i++;
-    } else {
-      console.log("No more entries");
-    }
-  };
-}
-
 
 /**
    * @param {string} locationid
@@ -167,8 +84,6 @@ function displayPubList(store) {
 function addCoordinates(locationid, timestamp, latitude, longitude) {
   console.log("addCoordinates arguments:", arguments);
     var obj = { locationid, timestamp, latitude, longitude };
-    if (typeof blob != 'undefined')
-      obj.blob = blob;
 
     var store = getObjectStore(DB_STORE_NAME, 'readwrite');
     var req;
@@ -176,18 +91,31 @@ function addCoordinates(locationid, timestamp, latitude, longitude) {
       req = store.add(obj);
     } catch (e) {
       if (e.name == 'DataCloneError')
-        displayActionFailure("This engine doesn't know how to clone a Blob, " +
+        console.log("This engine doesn't know how to clone a Blob, " +
                              "use Firefox");
       throw e;
     }
     req.onsuccess = function (evt) {
       console.log("Insertion in DB successful");
-      displayActionSuccess();
-      displayPubList(store);
     };
     req.onerror = function() {
       console.error("addPublication error", this.error);
-      displayActionFailure(this.error);
     };
 }
 
+function saveCheckPoint(locationid, timestamp, latitude, longitude) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  fetch(API_URL, {
+    method:'POST',
+    headers: myHeaders,
+    body: {
+          id: locationid,
+          latitude,
+          longitude,
+          timestamp
+      }
+  }).then(res => {
+    console.log(res.json());
+  });
+}
