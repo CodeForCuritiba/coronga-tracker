531ab3b9c2fac6ced3b86f9d16702ec01c7259b4
Adão Lima <adaoolima@gmail.com>
2020-10-18 22:10:05 -0300

    Added listener to compare history

--- frontend/static/handleBehaviors.js
+++ frontend/static/handleBehaviors.js
@@ -1,8 +1,9 @@
 
   function addEventListeners() {
     console.log("...adding EventListeners");
-    document.getElementById("js-getLocation").addEventListener("click", getLocation);
-    document.getElementById("js-openDB").addEventListener("click", openDb);
+    document.getElementById("js-getLocation").addEventListener("click", getLocationAndSaveInDB);
+    // document.getElementById("js-openDB").addEventListener("click", openDb);
+    document.getElementById("js-compareHistory").addEventListener("click", compareHistory);
   }
 
   window.onload = addEventListeners();
